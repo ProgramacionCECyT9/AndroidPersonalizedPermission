9e5942ee5aeaa069d9d0b1df207ea45d07492024
César Lara <laralacesar@gmail.com>
2015-05-20 08:16:44 -0500

    Original teacher project

---
+++ Practica21_Seguridad/app/src/main/java/edu/cecyt9/ipn/practica21_seguridad/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.cecyt9.ipn.practica21_seguridad;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ Practica21_Seguridad/app/src/main/java/edu/cecyt9/ipn/practica21_seguridad/VerVideo.java
@@ -0,0 +1,18 @@
+package edu.cecyt9.ipn.practica21_seguridad;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class VerVideo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Reproduciendo Vídeo", Toast.LENGTH_SHORT).show();
+    }
+
+}
